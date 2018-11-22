a4ae20bccff08fc71bdbd3fe015a2d46a07a8e4c
Jackson Tian <shyvo1987@gmail.com>
2018-11-22 19:42:30 +0800

    trigger travis

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -1,13 +1,14 @@
 package org.jacoco.examples.maven.java;
 
 public class HelloWorld {
-	
+
 	public String getMessage(boolean bigger) {
 		if (bigger) {
 			return "Hello Universe!";
 		} else {
 			return "Hello World!";
 		}
+    return "never executed!";
 	}
 
 }
